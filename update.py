9fe5bd6886403758f177d3222c8b7484f598622d
Breyon Gunn <superlazer@icloud.com>
2023-12-14 15:15:15 -0600

    Add config.json

--- update.py
+++ update.py
@@ -1,6 +1,12 @@
+import json
+
 import requests
 import os
 
+
+class Config:
+    def __init__(self, minecraft_version: str):
+        self.minecraft_version = minecraft_version
 
 class Change:
     def __init__(self, commit, summary, message):
@@ -35,9 +41,26 @@ class Project:
         self.builds = [Build(**build) for build in builds]
 
 
+def load_config() -> Config:
+    try:
+        with open("config.json", "r") as f:
+            json_data = json.load(f)
+            f.close()
+
+        return Config(minecraft_version=json_data["minecraftVersion"])
+    except FileNotFoundError:
+        with open("config.json", "w") as f:
+            json.dump({"minecraftVersion": "1.20.1"}, f)
+            f.close()
+        return Config(minecraft_version="1.20.1")
+
+
 def main():
+
+    config = load_config()
+
     # Fetch JSON data from the API
-    url = "https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds"
+    url = f"https://api.papermc.io/v2/projects/paper/versions/{config.minecraft_version}/builds"
     response = requests.get(url)
     json_data = response.json()
 
@@ -72,7 +95,7 @@ def main():
 
     # Download the latest build
     print("Downloading latest build...")
-    url = (f"https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/{last_build.build}/downloads/"
+    url = (f"https://api.papermc.io/v2/projects/paper/versions/{config.minecraft_version}/builds/{last_build.build}/downloads/"
            f"paper-1.20.1-{last_build.build}.jar")
     response = requests.get(url)
     with open("paper.jar", "wb") as f:
